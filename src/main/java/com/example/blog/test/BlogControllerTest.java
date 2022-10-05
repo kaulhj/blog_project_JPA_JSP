842fa261e14a57699b69c4fd85d5fe23ca982afe
Jinger <lhj69382@naver.com>
2022-10-05 18:04:09 +0900

    first commit

---
+++ src/main/java/com/example/blog/test/BlogControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  //스캔해서 모든 파일 메모리 new하는 것이 아니라 특정 어노테이션이 붙어있는 클래스 파일들을 new(ioc)해서 스프링 컨테이너에 관리
+public class BlogControllerTest {
+
+    @GetMapping("/test/hello")
+    public String hello(){
+        return "<h1>hello spring boot<h1>";
+    }
+}
